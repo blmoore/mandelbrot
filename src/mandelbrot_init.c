648f360da4329ea719e1febe026fb94df05dc049
Ben Moore <b3nmoore@gmail.com>
2017-06-25 19:44:07 +0100

    notes

---
+++ src/mandelbrot_init.c
@@ -0,0 +1,20 @@
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME:
+ Check these declarations against the C/Fortran source code.
+ */
+
+/* .C calls */
+extern void mandelbrot_(void *, void *, void *, void *, void *, void *);
+
+static const R_CMethodDef CEntries[] = {
+  {"mandelbrot_", (DL_FUNC) &mandelbrot_, 6},
+  {NULL, NULL, 0}
+};
+
+void R_init_mandelbrot(DllInfo *dll)
+{
+  R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}
