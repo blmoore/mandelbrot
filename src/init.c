9521a5be85d08d5edf218db0f560da02d8c05d0f
Ben Moore <b3nmoore@gmail.com>
2017-07-21 23:14:22 +0100

    dedupe c (#4)

--- src/init.c
+++ src/init.c
@@ -7,11 +7,9 @@
 
 /* .C calls */
 extern void mandelbrot_(void *, void *, void *, void *, void *, void *);
-extern void mandelbrot_alt(void *, void *, void *, void *, void *, void *);
 
 static const R_CMethodDef CEntries[] = {
   {"mandelbrot_",    (DL_FUNC) &mandelbrot_,    6},
-  {"mandelbrot_alt", (DL_FUNC) &mandelbrot_alt, 6},
   {NULL, NULL, 0}
 };
 
